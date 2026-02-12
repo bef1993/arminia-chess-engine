3675a77e96518d8bae4622a9815e25fd0370be14
Stefan Wilfinger <bef@gmx.net>
2026-02-12 21:13:55 +0100

    Improve material counting

--- internal/search/eval.go
+++ internal/search/eval.go
@@ -5,28 +5,30 @@ import "arminia-chess-engine/internal/engine"
 // Evaluate calculates the score of the current board position from the perspective
 // of the current player. A positive score means the current player has an advantage.
 func Evaluate(game *engine.Game) int {
-	// This is an absolute score from White's perspective.
-	// Positive means White is winning, negative means Black is winning.
-	score := 0
 
+	score := countMaterial(game.Board)
+
+	// Convert the absolute score to a perspective-based score.
+	if game.CurrentTurn == engine.White {
+		return score
+	}
+	return -score
+}
+
+// Counts the material on the board and returns a score.
+// Positive values indicate an advantage for White,
+// negative values indicate an advantage for Black.
+func countMaterial(board *engine.Board) (score int) {
 	for row := 0; row < 8; row++ {
 		for col := 0; col < 8; col++ {
-			piece := game.Board.GetPiece(col, row)
-			if piece == engine.NoPiece {
-				continue
-			}
-
+			piece := board.GetPiece(col, row)
 			if piece.Color() == engine.White {
 				score += piece.Value()
 			} else {
 				score -= piece.Value()
 			}
-		}
-	}
 
-	// Convert the absolute score to a perspective-based score.
-	if game.CurrentTurn == engine.Black {
-		return -score
+		}
 	}
 	return score
 }
