a5fdebfa6e4671bee62ed4c5bc993f2efc427bbd
Stefan Wilfinger <bef@gmx.net>
2026-02-15 16:30:00 +0100

    Improve countMaterial by using bitboards

--- internal/search/eval.go
+++ internal/search/eval.go
@@ -17,6 +17,7 @@ const (
 
 // Evaluate calculates the score of the current board position from the perspective
 // of the current player. A positive score means the current player has an advantage.
+// TODO : add more evaluation factors (piece-square tables, mobility, king safety, etc.)
 func Evaluate(game *engine.Game) int {
 
 	score := countMaterial(game.Board)
@@ -31,17 +32,13 @@ func Evaluate(game *engine.Game) int {
 // Counts the material on the board and returns a score.
 // Positive values indicate an advantage for White,
 // negative values indicate an advantage for Black.
-func countMaterial(board *engine.Board) (score int) {
-	for sq := 0; sq < 64; sq++ {
-		piece := board.GetPiece(sq)
-		if piece == engine.NoPiece {
-			continue
-		}
-		if piece.Color() == engine.White {
-			score += piece.Value()
-		} else {
-			score -= piece.Value()
-		}
-	}
+func countMaterial(board *engine.Board) int {
+	score := 0
+	score += (board.Pieces[engine.White][engine.Pawn].Count() - board.Pieces[engine.Black][engine.Pawn].Count()) * engine.PawnValue
+	score += (board.Pieces[engine.White][engine.Knight].Count() - board.Pieces[engine.Black][engine.Knight].Count()) * engine.KnightValue
+	score += (board.Pieces[engine.White][engine.Bishop].Count() - board.Pieces[engine.Black][engine.Bishop].Count()) * engine.BishopValue
+	score += (board.Pieces[engine.White][engine.Rook].Count() - board.Pieces[engine.Black][engine.Rook].Count()) * engine.RookValue
+	score += (board.Pieces[engine.White][engine.Queen].Count() - board.Pieces[engine.Black][engine.Queen].Count()) * engine.QueenValue
+	score += (board.Pieces[engine.White][engine.King].Count() - board.Pieces[engine.Black][engine.King].Count()) * engine.KingValue
 	return score
 }
